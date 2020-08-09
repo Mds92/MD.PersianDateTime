741f4e5757d87a70c0c1810910aca4cedac14759
Mohammad Dayyan <mds_soft@yahoo.com>
2020-08-09 19:47:10 +0430

    Add test project

--- MD.PersianDateTime.Standard.Test/UnitTest1.cs
+++ MD.PersianDateTime.Standard.Test/UnitTest1.cs
@@ -5,10 +5,24 @@ namespace MD.PersianDateTime.Standard.Test
     public class UnitTest1
     {
         [Fact]
-        public void GetPersianDateOfLastDayOfMonth()
+        public void GetPersianDateOfLastDayOfMonth1()
         {
             var date = new PersianDateTime(1399, 5, 1);
-            Assert.Equal(new PersianDateTime(1399, 5, 31), date.GetPersianDateOfLastDayOfMonth());
+            Assert.Equal(new PersianDateTime(date.Year, date.Month, date.GetMonthDays), date.GetPersianDateOfLastDayOfMonth());
+        }
+
+        [Fact]
+        public void GetPersianDateOfLastDayOfMonth2()
+        {
+            var date = new PersianDateTime(1399, 10, 1);
+            Assert.Equal(new PersianDateTime(date.Year, date.Month, date.GetMonthDays), date.GetPersianDateOfLastDayOfMonth());
+        }
+
+        [Fact]
+        public void GetPersianDateOfLastDayOfMonth3()
+        {
+            var date = new PersianDateTime(1399, 12, 1);
+            Assert.Equal(new PersianDateTime(date.Year, date.Month, date.GetMonthDays), date.GetPersianDateOfLastDayOfMonth());
         }
     }
 }
